dc9a5624fe99f0e0c6225ba4336872b4037b7cdc
gkiserpong <gkiserpongit@gmail.com>
2022-04-30 14:53:56 +0700

    Email password in settings

--- config/settings.py
+++ config/settings.py
@@ -147,7 +147,8 @@ EMAIL_HOST = 'host.gkiserpong.org'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'noreply@gkiserpong.org'
-EMAIL_HOST_PASSWORD = str(os.getenv('EMAIL_HOST_PASSWORD'))
+#EMAIL_HOST_PASSWORD = str(os.getenv('EMAIL_HOST_PASSWORD'))
+EMAIL_HOST_PASSWORD = '2MzVugIPTf'
 
 
 
