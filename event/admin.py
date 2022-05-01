a318df77093daba69fbcce36d00105d0d07ef944
gkiserpong <gkiserpongit@gmail.com>
2022-05-02 03:11:25 +0700

    Clean commit - more

--- event/admin.py
+++ event/admin.py
@@ -3,9 +3,12 @@ from event.models import Event
 
 # Event
 class EventAdmin(admin.ModelAdmin):
-    fields = ['nama', 'info', 'lokasi', 'tanggal', 'kapasitas', 'jumlah_pendaftar', 'kehadiran', 'is_active']
+    fields = ['nama', 'info', 'lokasi', 'tanggal', 
+            'kapasitas', 'jumlah_pendaftar', 'kehadiran', 'is_active']
+    
     readonly_fields=('jumlah_pendaftar', 'kehadiran', 'is_active', )
+    
     list_display = ['id', 'nama', 'tanggal', 'lokasi',
-                    'kapasitas', 'jumlah_pendaftar']
+            'kapasitas', 'jumlah_pendaftar', 'is_active']
 
 admin.site.register(Event, EventAdmin)
--- registrant/admin.py
+++ registrant/admin.py
@@ -4,9 +4,15 @@ from .models import Registrant, Member
 
 
 class RegistrantAdmin(admin.ModelAdmin):
-    fields = ['nama', 'email', 'telepon', 'wilayah', 'event', 'jumlah', 'is_come', 'is_active']
-    readonly_fields=('nama', 'email', 'telepon', 'wilayah', 'event', 'jumlah', 'is_come', 'is_active', )
-    list_display = ('id', 'nama', 'email', 'telepon', 'wilayah', 'event', 'jumlah', 'is_come', 'is_active')
+    fields = ['nama', 'email', 'telepon', 'wilayah', 
+            'event', 'jumlah', 'is_come', 'is_active']
+    
+    readonly_fields=('nama', 'email', 'telepon', 
+            'wilayah', 'event', 'jumlah', 'is_come', 'is_active', )
+    
+    list_display = ('id', 'nama', 'email', 'telepon', 'wilayah', 
+            'event', 'jumlah', 'is_come', 'is_active')
+    
     list_filter = ('event',)
 
 #class MemberAdmin(admin.ModelAdmin):
