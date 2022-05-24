a4d91c659b69cde52b8b2af27f02d71c772d33d1
gkiserpong <gkiserpongit@gmail.com>
2022-05-24 13:58:07 +0700

    No Add and Delete for Pin

--- qrscan/admin.py
+++ qrscan/admin.py
@@ -6,5 +6,14 @@ class PinAdmin(admin.ModelAdmin):
     class Meta:
         model = Pin
 
+    # This will help you to disbale add functionality
+    def has_add_permission(self, request):
+        return False
+
+    # This will help you to disable delete functionaliyt
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
 
 admin.site.register(Pin, PinAdmin)
