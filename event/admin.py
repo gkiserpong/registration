dc2fb67c759471456eab48b82cc36eb82daec004
gkiserpong <gkiserpongit@gmail.com>
2022-06-01 00:29:46 +0700

    Disable edit event when is_active=False

--- event/admin.py
+++ event/admin.py
@@ -11,4 +11,10 @@ class EventAdmin(admin.ModelAdmin):
     list_display = ['nama', 'tanggal', 'lokasi',
             'kapasitas', 'jumlah_pendaftar', 'kehadiran', 'is_active']
 
+    def has_change_permission(self, request, obj=None):
+        if obj is not None and not obj.is_active:
+            return False
+        return super().has_change_permission(request, obj=obj)
+
+
 admin.site.register(Event, EventAdmin)
